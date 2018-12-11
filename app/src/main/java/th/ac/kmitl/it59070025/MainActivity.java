795f701474168d52d47b87d7be5f6e9c69eeffba
koungkmitl <59070025@it.kmitl.ac.th>
2018-12-11 09:46:46 +0700

    init code

---
+++ app/src/main/java/th/ac/kmitl/it59070025/MainActivity.java
@@ -0,0 +1,21 @@
+package th.ac.kmitl.it59070025;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_view, new LoginFragment())
+                    .commit()
+            ;
+        }
+    }
+}
---
+++ app/src/main/java/th/ac/kmitl/it59070025/auth/LoginFragment.java
@@ -0,0 +1,30 @@
+package th.ac.kmitl.it59070025.auth;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.ac.kmitl.it59070025.R;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    
+}
